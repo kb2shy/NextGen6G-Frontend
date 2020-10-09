1a69ae9622d52c61d89fa542cdbe1e8932931b0c
Chhaian Pin <chhaianpin@hotmail.com>
2020-10-09 10:54:17 -0700

    deleted JSON lottie files that were not compatible with react native project. integrated geolocation.

--- App.js
+++ App.js
@@ -1,30 +1,73 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Image, Text, View } from 'react-native';
 import LottieView from 'lottie-react-native';
 import * as Speech from 'expo-speech';
+import * as Location from 'expo-location';
 
-const App = () => {
+const TEXT_SCRIPTS = {
+  welcome: "Welcome to Co-Pilot B9. I'll help you stay alert by letting you know when we reach high accident areas.",
+  noAlert: "It Looks like we're in the clear. Let's stay alert and I'll let you know when we reach another high accident area.",
+  alert: "Now entering a high accident area. Let's stay alert!",
+  distracted: "Did you just get distracted?"
+}
+
+export default function App() {
+
+  // State for alert, text
+  const [alertState, setAlertState] = useState("");
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestPermissionsAsync();
+      if (status !== 'granted') {
+        return setErrorMsg('Permission to access location was denied');
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      console.log(location);
+      setLocation(location);
+    })();
+  }, [])
+
+
+
+  // Function to return 
   
-  const sayWelcome = () => {
-    Speech.speak("Welcome to Co-Pilot B9. I'll help you stay alert by letting you know when we reach high accident areas.");
+  const getSpeak = () => {
+    switch(alertState) {
+      case "welcome":
+        return Speech.speak(TEXT_SCRIPTS.welcome);
+      case "noAlert": 
+        return Speech.speak(TEXT_SCRIPTS.noAlert);
+      case "alert":
+        return Speech.speak(TEXT_SCRIPTS.alert);
+      case "distracted":
+        return Speech.speak(TEXT_SCRIPTS.distracted);
+      default:
+        return Speech.speak(TEXT_SCRIPTS.welcome);
+    }
   }
 
   return (
     <View style={styles.container}>
       <View style={styles.containerHeader}>
         <Text style={styles.header}>Welcome to Co-Pilot B9</Text>
-        <Text style={styles.paragraph}>I'll help you stay alert by letting you know when we reach high accident areas.</Text>
+        {/* <Text style={styles.paragraph}>I'll help you stay alert by letting you know when we reach high accident areas.</Text> */}
+        {/* <Text style={styles.paragraph}>Now entering a high accident area. Let's stay alert!</Text> */}
+        <Text style={styles.paragraph}>Did you just get distracted?</Text>
       </View>
       <View style={styles.lottieView}>
         <LottieView
           style={styles.lottiView}
-          source={require('./assets/car2.json')}
+          source={require('./assets/attention2.json')}
           autoPlay
           loop
         />
       </View>
-      {sayWelcome()}
+      {getSpeak()}
     </View>
   );
 }
@@ -58,5 +101,3 @@ const styles = StyleSheet.create({
     padding: 20
   }
 });
-
-export default App;
