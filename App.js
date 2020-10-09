241fcbda9719969b974d75085fa2bd630105cca9
Chhaian Pin <chhaianpin@hotmail.com>
2020-10-09 12:47:14 -0700

    added switch statements to dynamically render text display and text to speech.

--- App.js
+++ App.js
@@ -6,7 +6,8 @@ import * as Speech from 'expo-speech';
 import * as Location from 'expo-location';
 
 const TEXT_SCRIPTS = {
-  welcome: "Welcome to Co-Pilot B9. I'll help you stay alert by letting you know when we reach high accident areas.",
+  welcome: "Welcome to Co-Pilot B9.",
+  welcome2: "I'll help you stay alert by letting you know when we reach high accident areas.",
   noAlert: "It Looks like we're in the clear. Let's stay alert and I'll let you know when we reach another high accident area.",
   alert: "Now entering a high accident area. Let's stay alert!",
   distracted: "Did you just get distracted?"
@@ -15,7 +16,7 @@ const TEXT_SCRIPTS = {
 export default function App() {
 
   // State for alert, text
-  const [alertState, setAlertState] = useState("");
+  const [alertState, setAlertState] = useState("distracted");
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
 
@@ -26,20 +27,29 @@ export default function App() {
         return setErrorMsg('Permission to access location was denied');
       }
 
-      let location = await Location.getCurrentPositionAsync({});
+      let currentLocation = await Location.getCurrentPositionAsync({});
+
+      // set location if location === null
+      if (!location) {
+        setLocation(currentLocation);
+      }
+      
+      // calculate difference between currentLocation and state location to update state location
+      const dLongitude = Math.abs(currentLocation.coords.longitude - location.coords.longitude);
+      const dLatitude = Math.abs(currentLocation.coords.latitude - location.coords.latitude);
+      if (dLongitude > 0.000005 || dLatitude > 0.000005) {
+        setLocation(currentLocation);
+      }
+
       console.log(location);
-      setLocation(location);
     })();
-  }, [])
+  }, []);
 
-
-
-  // Function to return 
-  
+  // Function to return text to speech  
   const getSpeak = () => {
     switch(alertState) {
-      case "welcome":
-        return Speech.speak(TEXT_SCRIPTS.welcome);
+      case "welcome2":
+        return Speech.speak(TEXT_SCRIPTS.welcome2);
       case "noAlert": 
         return Speech.speak(TEXT_SCRIPTS.noAlert);
       case "alert":
@@ -47,7 +57,38 @@ export default function App() {
       case "distracted":
         return Speech.speak(TEXT_SCRIPTS.distracted);
       default:
+        setAlertState("welcome2");
         return Speech.speak(TEXT_SCRIPTS.welcome);
+    }
+  }
+
+  // Function to return text below header
+  const getText = () => {
+    switch(alertState) {
+      case "welcome2":
+        return <Text style={styles.paragraph}>{TEXT_SCRIPTS.welcome2}</Text>;
+      case "noAlert":
+        return <Text style={styles.paragraph}>{TEXT_SCRIPTS.noAlert}</Text>;
+      case "alert":
+        return <Text style={styles.paragraph}>{TEXT_SCRIPTS.alert}</Text>;
+      case "distracted":
+        return <Text style={styles.paragraph}>{TEXT_SCRIPTS.distracted}</Text>;
+      default:
+        return;
+    }
+  }
+
+  // Function to update React Native Lottie animation
+  const getAnimation = () => {
+    switch(alertState) {
+      case "noAlert":
+        return <LottieView style={styles.lottie} source={require('./assets/car2.json')} autoPlay loop />
+      case "alert":
+        return <LottieView style={styles.lottie} source={require('./assets/warning3.json')} autoPlay loop />
+      case "distracted":
+        return <LottieView style={styles.lottie} source={require('./assets/attention2.json')} autoPlay loop />
+      default:
+        return <LottieView style={styles.lottie} source={require('./assets/car2.json')} autoPlay loop />;
     }
   }
 
@@ -55,17 +96,10 @@ export default function App() {
     <View style={styles.container}>
       <View style={styles.containerHeader}>
         <Text style={styles.header}>Welcome to Co-Pilot B9</Text>
-        {/* <Text style={styles.paragraph}>I'll help you stay alert by letting you know when we reach high accident areas.</Text> */}
-        {/* <Text style={styles.paragraph}>Now entering a high accident area. Let's stay alert!</Text> */}
-        <Text style={styles.paragraph}>Did you just get distracted?</Text>
+        {getText()}
       </View>
       <View style={styles.lottieView}>
-        <LottieView
-          style={styles.lottiView}
-          source={require('./assets/attention2.json')}
-          autoPlay
-          loop
-        />
+        {getAnimation()}
       </View>
       {getSpeak()}
     </View>
@@ -96,6 +130,10 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     height: "60%",
     width: "100%",
+  },
+  lottie: {
+    height: "100%",
+    width: "100%"
   },
   paragraph: {
     padding: 20
